ca5cfec0f380f414abe01a5e09538b8386411e50
Abdi Mussa <abdimussa87@gmail.com>
2024-01-05 23:53:16 +0300

    feat: chat bot now creates visualizations using redash api

--- redash/redash/handlers/chat.py
+++ redash/redash/handlers/chat.py
@@ -3,18 +3,33 @@ from redash.handlers.base import (
     BaseResource
 )
 import os
+from redashAPI import RedashAPIClient
+
 from openai import OpenAI
 VARIABLE_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(
   api_key=VARIABLE_KEY
 )
+REDASH_API_KEY = os.environ.get('REDASH_API_KEY')
+
 
 class ChatResource(BaseResource):
     def post(self):
         try:
             value = request.get_json()
             question = value.get('question')
-            completion = client.chat.completions.create(
+            result = self.get_sql_query(question)
+            is_sql  = self.check_if_valid_sql(result)
+            if(is_sql):
+                self.perform_redash_request(result)
+            response_data = {"answer": result}
+            return jsonify(response_data), 200
+        except Exception as error:
+            print(error)
+            return jsonify({"error": "An error occurred"}), 500
+        
+    def get_sql_query(self,question):    
+        completion = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "system",
@@ -51,9 +66,35 @@ class ChatResource(BaseResource):
                     {"role": "user", "content": question}
                 ]
             )
-            answer = completion.choices[0].message.content
-            response_data = {"answer": answer}
-            return jsonify(response_data), 200
-        except Exception as error:
-            print(error)
-            return jsonify({"error": "An error occurred"}), 500
+        sql_answer = completion.choices[0].message.content
+        return sql_answer
+    
+    def check_if_valid_sql(self,question):    
+        completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system",
+                    "content": """You are a SQL MASTER. You know if the provided input is a valid SQL or not. If the provided input is NOT a valid input you return FALSE. If the provided input is a valid input you return TRUE. MAKE SURE YOUR ANSWER IS ONLY EITHER TRUE OR FALSE. NOTHING ELSE. NOT EVEN PUNCTUATION MARKS"""
+                    },
+                    {"role": "user", "content": question}
+                ]
+            )
+        is_valid_sql = completion.choices[0].message.content
+        return is_valid_sql
+    
+    def perform_redash_request(self,query_string):
+        Redash = RedashAPIClient(REDASH_API_KEY, 'http://localhost:5000')
+        res = Redash.get('data_sources')
+        data_source_id = res.json()[0]['id']
+        res = Redash.create_query(data_source_id, "Query", query_string)
+        query_id = res.json()['id']
+        res = Redash.create_visualization(query_id, "table", "Visualization", columns=[
+    {"name": "column 1", "type": "string"}
+])
+        visualization_id  = res.json()['id']
+        res = Redash.get('dashboards')
+        dashboard_id = res.json()['results'][0]['id']
+        Redash.add_widget(dashboard_id, vs_id=visualization_id,position={"col": 0,
+            "row": 0,
+            "sizeX": 3,
+            "sizeY": 8})
